8951fe40be53d6c4300b721ce1f2d10767e4e4e3
swfree <skye.free@gmail.com>
2016-03-16 17:19:51 -0700

    Pass several test specs

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -1,7 +1,43 @@
 // SongQueue.js - Defines a backbone model class for the song queue.
 var SongQueue = Songs.extend({
 
+  queuedSongs: [],
   initialize: function() {
-  }
+    this.on('enqueue', this.enqueue);
+    this.on('ended', this.remove, this);
+  },
+
+  add: function(newSong) {
+    // console.log(this.model.get('songQueue'));
+    // this.get('storeSongQueue');
+
+    var model = new SongModel(songData);
+
+    this.queuedSongs.push(model);
+    if (this.queuedSongs.length === 1) {
+      this.playFirst();
+      
+    }
+    // add newSong to queue
+    // init an array that stores new songs we pass in
+    // array[0] will be current song
+    // call PlayFirst --> playView.setSong(first song)
+    // whenever current song ends
+    // get rid of current song
+    // move up next song
+  },
+
+  remove: function() {
+    this.queuedSongs.splice(0, 1);
+  },
+
+  // this.queuedSongs.on('ended', this)
+    // this.queuedSongs.splice(0)
+
+  enqueue: function() {
+
+  },
+
+  playFirst: function() {}
 
 });
--- client/views/LibraryEntryView.js
+++ client/views/LibraryEntryView.js
@@ -8,6 +8,9 @@ var LibraryEntryView = Backbone.View.extend({
   events: {
     'click': function() {
       this.model.play();
+      console.log(this);
+      console.log(this.model);
+      this.model.enqueue(this.model);
     }
   },
 
